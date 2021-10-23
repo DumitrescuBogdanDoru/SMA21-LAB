97c0b72d8f6725ff659508dd0895c3c73950f5b9
DumitrescuBogdanDoru <bogdandoru.dumitrescu@gmail.com>
2021-10-23 12:37:16 +0300

    Empty Project

---
+++ SMA/app/src/main/java/com/sma/websearch/WebsearchActivity.java
@@ -0,0 +1,16 @@
+package com.sma.websearch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sma.R;
+
+public class WebsearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_websearch);
+    }
+}
