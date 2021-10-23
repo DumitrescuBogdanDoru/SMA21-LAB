f6d448ae2894f3171128825088b81cf428ea8165
DumitrescuBogdanDoru <bogdandoru.dumitrescu@gmail.com>
2021-10-23 17:13:05 +0300

    Task-2: partial implementation

---
+++ SMA/app/src/main/java/com/sma/DownloadImageTask.java
@@ -0,0 +1,38 @@
+package com.sma;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import com.sma.service.ForegroundImageService;
+
+public class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+    private Context context;
+
+
+    public DownloadImageTask(Context context) {
+        this.context = context;
+        Toast.makeText(context, "Please wait, it may take a few seconds.", Toast.LENGTH_SHORT).show();
+    }
+
+    public DownloadImageTask() {
+
+    }
+
+    protected Bitmap doInBackground(String... urls) {
+
+        Bitmap bmp = null;
+
+        return bmp;
+
+
+    }
+
+    protected void onPostExecute(Bitmap result) {
+        // save bitmap result in application class
+
+        // send intent to stop foreground service
+
+    }
+}
--- SMA/app/src/main/java/com/sma/websearch/ImageActivity.java
+++ SMA/app/src/main/java/com/sma/websearch/ImageActivity.java
@@ -3,7 +3,10 @@ package com.sma.websearch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.sma.MyApplication;
 import com.sma.R;
 
 public class ImageActivity extends AppCompatActivity {
@@ -12,5 +15,15 @@ public class ImageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image);
+
+        // check if application has expected payload
+        MyApplication myApplication = (MyApplication) getApplicationContext();
+        if (myApplication.getBitmap() == null) {
+            Toast.makeText(this, "Error transmitting URL.", Toast.LENGTH_SHORT).show();
+            finish();
+        } else {
+            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+            imageView.setImageBitmap(myApplication.getBitmap());
+        }
     }
 }
